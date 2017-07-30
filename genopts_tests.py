b8d1427fe5a15175941bba1a89adb5859f1a829f
Sebastian Bauer <mail@sebastianbauer.info>
2017-07-30 21:08:24 +0200

    Add a first unittest of the parser.

--- genopts_tests.py
+++ genopts_tests.py
@@ -5,9 +5,20 @@
 # Unit tests for genopts.py
 #
 
+from __future__ import print_function
+
 from genopts import *
 
 import unittest
+
+class TestParser(unittest.TestCase):
+    def test_parse_optional_simple(self):
+        rem, parse_tree = parse_optional("[--option]")
+        self.assertEquals('', rem)
+        self.assertTrue(isinstance(parse_tree, Optional))
+        self.assertEquals(1, len(parse_tree.list))
+        self.assertTrue(isinstance(parse_tree.list[0], OptionWithArg))
+        self.assertEquals("--option", parse_tree.list[0].command)
 
 if __name__ == "__main__":
     unittest.main()
