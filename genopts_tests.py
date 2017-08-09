ffa9799167ed1f97d9e952cef0897d697d7019fd
Sebastian Bauer <mail@sebastianbauer.info>
2017-08-09 07:53:52 +0200

    Add test for parsing two patterns.

--- genopts_tests.py
+++ genopts_tests.py
@@ -50,5 +50,30 @@ class TestParser(unittest.TestCase):
         self.assertEquals("--option", parse_tree.list[0].command)
         self.assertEquals("--no-option", parse_tree.list[1].command)
 
+    def test_parse_two_patterns(self):
+        # type: () -> None
+        parse_tree = []
+        parse_tree.append(parse_pattern("[--common-option] cmd1 [--same] [--cmd1-option]"))
+        parse_tree.append(parse_pattern("[--common-option] cmd2 [--same] [--cmd2-option]"))
+        template = Template(parse_tree)
+        gf = GenFile(f=open("/dev/zero","w"))
+        field_names = dict() # type: Dict[str, str]
+        command_index_map = CommandIndexMap()
+        parent_map = ParentMap()
+        token_action_map = TokenActionMap()
+        navigate(template, GenerateParserVisitor(field_names, command_index_map, parent_map, token_action_map))
+
+        gf = GenFile()
+        self.assertTrue("--common-option" in token_action_map)
+        self.assertTrue("--same" in token_action_map)
+        self.assertTrue("--cmd1-option" in token_action_map)
+        self.assertTrue("--cmd2-option" in token_action_map)
+        self.assertTrue("cmd1" in token_action_map)
+        self.assertTrue("cmd2" in token_action_map)
+
+        options = [] # type: List[OptionWithArg]
+        navigate(template, OptionWithArgExtractorVisitor(True, options))
+        self.assertEquals(4, len(options))
+
 if __name__ == "__main__":
     unittest.main()
