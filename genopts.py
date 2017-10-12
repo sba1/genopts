a64840109a9b4478a691987e2f49ec51062bf48f
Sebastian Bauer <mail@sebastianbauer.info>
2017-10-12 22:58:18 +0200

    Fix a printerr() call.

--- genopts.py
+++ genopts.py
@@ -941,7 +941,7 @@ def genopts(patterns):
         for a in all_args:
             if a.command not in optional_args:
                 vc.iff(cond="!cli->{0}".format(makecname(a.command))).then. \
-                    printerr("Required argument \\\"{0}\\\" is missing. Use --help for usage\\n\");".format(a.command)). \
+                    printerr("Required argument \\\"{0}\\\" is missing. Use --help for usage\\n".format(a.command)). \
                     ret(0)
 
         vc.add("}")
