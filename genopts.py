acc3d4dc06158d330923e89651fcd792690c1a10
Sebastian Bauer <mail@sebastianbauer.info>
2017-11-25 20:41:41 +0100

    Use cli_access().

--- genopts.py
+++ genopts.py
@@ -1152,7 +1152,9 @@ def genopts(patterns, backend):
         if not any(a.variadic for a in all_args) and len(all_args) == 2 and len(optional_args) == 1 and all_args[0].command in optional_args:
             then = vc.iff(cond="aux->positional{0} != NULL".format(1)).then
             for pos, arg in enumerate(commands[1]):
-                then.add(make_expr("cli->{0}".format(makecname(arg.command))) << make_expr("aux->positional{0}".format(pos)))
+                cmd_name = makecname(arg.command)
+
+                then.add(cli_access(cmd_name) << make_expr("aux->positional{0}".format(pos)))
             then.otherwise().add("cli->{0} = aux->positional{1};".format(makecname(all_args[1].command), 0))
         else:
             # Resolve positional arguments
